3cc37f87645eb9512df3170e23cd658c120ddcc0
keioni <devel@keioni.com>
2025-03-24 01:49:54 +0900

    fix bugs and add skip existing files

--- flac_to_aac.sh
+++ flac_to_aac.sh
@@ -4,9 +4,9 @@ IFS=$'\n'
 
 find in -type d | sort > dir_list.txt
 for line in $(cat dir_list.txt); do
-    dir=$(echo "$line" | perl -lpe 's|^in/||')
-    echo "mkdir: .out/$dir"
-    mkdir -p "./out/$dir"
+    dir=$(echo "$line" | perl -lpe 's|^in/|out/|')
+    echo "mkdir: ./$dir"
+    mkdir -p "./$dir"
 done
 
 find in -type f -name '*.flac' | sort > flac_list.txt
@@ -14,7 +14,12 @@ total_number=$(cat flac_list.txt | wc -l | perl -lpe 's/ //g')
 current_count=1
 for line in $(cat flac_list.txt); do
     file_base=$(echo "$line" | perl -lpe 's|in/||' | perl -lpe 's|.flac$||')
-    echo "[${current_count}/${total_number}] ffmpeg: ${file_base}.flac"
-    ffmpeg -y -v 24 -i "in/${file_base}.flac" -c:v copy -c:a aac_at -q:a 2 "out/${file_base}.m4a"
+    echo -n "[${current_count}/${total_number}]: ${file_base} ..."
+    if [ -f "out/${file_base}.m4a" ]; then
+        echo " skipped."
+    else
+        ffmpeg -y -v 24 -i "in/${file_base}.flac" -c:v copy -c:a aac_at -q:a 2 "out/${file_base}.m4a"
+        echo ""
+    fi
     current_count=$(expr $current_count + 1)
 done
