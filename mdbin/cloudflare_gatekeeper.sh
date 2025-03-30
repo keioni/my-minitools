f4b4b5520bb7772b84d503c143eeacf88bf7402d
keioni <devel@keioni.com>
2025-03-30 15:20:49 +0900

    add dry-run mode

--- mdbin/cloudflare_gatekeeper.sh
+++ mdbin/cloudflare_gatekeeper.sh
@@ -6,10 +6,14 @@ export ADDR_LIST_FILE="cloudflare_ipv4.txt"
 if [ "$1" eq "open" ]; then
     command="allow"
 elif [ "$1" eq "close" ]; then
-    command="deny"
+    command="delete allow"
 else
     echo "Usage: $0 open|close"
     exit 1
+fi
+
+if [ "$2" eq "--dry" ]; then
+    flag="--dry"
 fi
 
 # first, use current directory. second, use script directory.
@@ -39,10 +43,15 @@ for network in $(cat $ADDR_LIST_PATH); do
     # open or close port 443 for the cloudflare network
     if [ "$command" eq "allow" ]; then
         echo "> opening port 443 for $network ..."
-        ufw allow proto tcp from "$network" to any port 443
     else
         echo "> closing port 443 for $network ..."
-        ufw delete allow proto tcp from "$network" to any port 443
+    fi
+
+    # dry run?
+    if [ "$flag" eq "--dry" ]; then
+        echo "ufw $command proto tcp from $network to any port 443"
+    else
+        ufw $command proto tcp from "$network" to any port 443
     fi
 
 done
