4e2139af40c0399a9241fc0f3af19517488d188d
keioni <devel@keioni.com>
2025-04-02 16:17:47 +0900

    fix bugs

--- passgen.sh
+++ passgen.sh
@@ -4,11 +4,12 @@ LENGTH=${1:-16}
 
 while true; do
     PASSWORD=$(cat /dev/urandom | LANG=C tr -dc 'A-Za-z0-9' | head -c "$LENGTH")
-    echo $PASSWORD | perl -lne 'print if (/[A-Z]/ && /[a-z]/ && /[0-9]/)'
+    echo $PASSWORD | perl -lne 'exit 1 unless (/[A-Z]/ && /[a-z]/ && /[0-9]/)'
     if [ $? -eq 0 ]; then
+        echo "Generated password: $PASSWORD"
         break
     else
         echo "Password does not meet criteria, generating a new one..."
-        sleep 1
+        sleep 0.1s
     fi
 done
