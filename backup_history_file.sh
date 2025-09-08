ea09c1797e90873fe5f80fbfb02beb1dce2be323
keioni <devel@keioni.com>
2025-09-08 11:42:10 +0900

    add color

--- backup_history_file.sh
+++ backup_history_file.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -eu
+#!/bin/bash
 
 # 84600 = 1 day * 7 = 1 week
 BACKUP_PERIOD=604800
@@ -6,18 +6,28 @@ HOST_NAME=$(hostname -s)
 DEST_PATH=$HOME/workspace/var/shell_histories
 INDICATOR_FILE=$DEST_PATH/_${HOST_NAME}_latest_backup_timestamp
 
+RED=$'\e[31m'
+GREEN=$'\e[32m'
+LIGHT_BLUE=$'\e[34m'
+RESET=$'\e[0m'
+
+ok()    { echo "${LIGHT_BLUE}$*${RESET}"; }
+error() { echo "${RED}$*${RESET}"; }
+
 backup_history_file() {
     if [ ! -d $DEST_PATH ]; then
-        echo "[FAILED] Backup path had some problems." && exit 1
+        error "[✖] History file backup failed! (destination path not found)"
+        exit 1
     fi
     backup_file_name=${HOST_NAME}_$(date +'%Y%m%d')
 
     cp -n $HOME/.zsh_history $DEST_PATH/$backup_file_name
     gzip -f $DEST_PATH/$backup_file_name
     if [ $? -ne 0 ]; then
-        echo "[FAILED] Backup history file failed." && exit 1
+        error "[✖] History file backup failed! (gzip failed)"
+        exit 1
     fi
-    echo "** Backed up history file. **"
+    ok "[✔] History file backup completed."
     echo "$(date +'%s') $(date +'%y-%m-%d %H:%M:%S')" > $INDICATOR_FILE
 }
 
