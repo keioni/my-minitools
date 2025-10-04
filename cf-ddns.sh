cfe1a103d2b3ab326a6b1515364a9b8effa0d431
keioni <devel@keioni.com>
2025-10-04 22:24:33 +0900

    fix bugs

--- cf-ddns.sh
+++ cf-ddns.sh
@@ -2,7 +2,7 @@
 
 # Dynamic DNS update script for use with Cloudflare's API
 # Requires: curl, jq
-# Usage: ./ddns.sh FQDN [IP address]
+# Usage: ./ddns.sh FQDN [-4|-6]
 
 # Set API_TOKEN before running.
 # FQDN is the full domain name to update, e.g. home.example.com
@@ -13,40 +13,29 @@ if [ -z "${API_TOKEN:-}" ]; then
 fi
 
 if [ "$#" -lt 1 ] || [ "$#" -gt 2 ]; then
-    echo "Usage: $0 FQDN [IP address]"
+    echo "Usage: $0 FQDN [-4|-6]"
     exit 1
 fi
-
+    
 FQDN="$1"
-IP_ADDR="${2:-}"
 
-# I love IPv6 
-# but if you want IPv4, change this to "4"
-ip_version="6"
+# I love IPv6
+IP_VERSION="${2:--6}" # default to IPv6
 
 # ip.me is provided by Proton AG,
 # the maker of Proton Mail, VPN, and other privacy-focused services.
-if [ -n "$IP_ADDR" ]; then
-    echo $IP_ADDR | grep -q ":"
-    if [ $? -ne 0 ]; then
-        ip_record_type="A"
-    else
-        ip_record_type="AAAA"
-    fi
+if [ "$IP_VERSION" = "-4" ]; then
+    ip_address=$(curl -s -4 https://ip.me)
+    record_type="A"
 else
-    if [ "$ip_version" = "4" ]; then
-        IP_ADDR=$(curl --silent -4 https://ip.me)
-        ip_record_type="A"
-    else
-        IP_ADDR=$(curl --silent -6 https://ip.me)
-        ip_record_type="AAAA"
-    fi
+    ip_address=$(curl -s -6 https://ip.me)
+    record_type="AAAA"
 fi
 
 
-# get Account ID from API token
+# get Account ID from API token to check API token validity
 # note: this assumes the token is scoped to a single account
-account_id=$(curl --silent -X GET "https://api.cloudflare.com/client/v4/accounts" \
+account_id=$(curl -s "https://api.cloudflare.com/client/v4/accounts" \
     -H "Authorization: Bearer $API_TOKEN" \
     | jq -r '.result[0].id')
 if [ "$account_id" = "null" ]; then
@@ -56,32 +45,31 @@ fi
 
 # get Zone ID from domain name
 domain_name="${FQDN#*.}"
-zone_id=$(curl --silent "https://api.cloudflare.com/client/v4/zones/?name=$domain_name" \
+zone_id=$(curl -s "https://api.cloudflare.com/client/v4/zones/?name=$domain_name" \
     -H "Authorization: Bearer $API_TOKEN" \
-    | jq -r '.result[0].id')
+    | jq -r '.result[] | select(.name == "'"$domain_name"'") | .id')
 if [ "$zone_id" = "null" ]; then
     echo "Zone not found for $domain_name"
     exit 1
 fi
 
-# get DNS record ID from record name
-record_id=$(curl --silent "https://api.cloudflare.com/client/v4/zones/$zone_id/dns_records?name=$FQDN" \
+# get DNS record ID from record name and type
+record_id=$(curl -s "https://api.cloudflare.com/client/v4/zones/$zone_id/dns_records?name=$FQDN" \
     -H "Authorization: Bearer $API_TOKEN" \
-    | jq -r '.result[0].id')
-
+    | jq -r '.result[] | select(.type == "'"$record_type"'") | .id')
 if [ "$record_id" = "null" ]; then
-    echo "DNS record not found for $FQDN"
+    echo "DNS $record_type record not found for $FQDN"
     exit 1
 fi
 
-update DNS record with new IP address
-curl --silent "https://api.cloudflare.com/client/v4/zones/$zone_id/dns_records/$record_id" \
--X PATCH \
--H "Authorization: Bearer $API_TOKEN" \
--d '{
-        "type": "'"$ip_record_type"'",
+# update DNS record with new IP address
+curl -s "https://api.cloudflare.com/client/v4/zones/$zone_id/dns_records/$record_id" \
+    -X PATCH \
+    -H "Authorization: Bearer $API_TOKEN" \
+    -d '{
+        "type": "'"$record_type"'",
         "name": "'"$FQDN"'",
-        "content": "'"$IP_ADDR"'",
+        "content": "'"$ip_address"'",
         "ttl": 3600,
         "proxied": false
-}' | jq
+    }' | jq
